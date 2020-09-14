051ebc6109d3d1f861025f4db81f11c007e9b7b9
elcajo <46391075+elcajo@users.noreply.github.com>
2020-09-15 00:25:09 +0800

    Updates

---
+++ precache-manifest.ebb4ed81e79640fed4ca7924c86f77c2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f861e3224871c94c5f83396cf3f5cb46",
+    "url": "/biofriends/index.html"
+  },
+  {
+    "revision": "a395bba451d57df94350",
+    "url": "/biofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d0f10ef960bb540701ab",
+    "url": "/biofriends/static/css/main.bdd6f4b4.chunk.css"
+  },
+  {
+    "revision": "a395bba451d57df94350",
+    "url": "/biofriends/static/js/2.6b86da38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/biofriends/static/js/2.6b86da38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0f10ef960bb540701ab",
+    "url": "/biofriends/static/js/main.6e417cd4.chunk.js"
+  },
+  {
+    "revision": "67bca3b3c58bcd82b21d",
+    "url": "/biofriends/static/js/runtime-main.744048b4.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/biofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
